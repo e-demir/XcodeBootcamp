9598232458f2873ca697d0af48fc833d4e2c99f5
dev_demir <dev_demir@outlook.com>
2023-01-21 23:52:54 +0300

    Initial Commit

---
+++ XcodeBootcamp/XcodeBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeBootcampApp.swift
+//  XcodeBootcamp
+//
+//  Created by Emrullah Demir on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct XcodeBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
