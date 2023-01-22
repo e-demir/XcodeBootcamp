4e27dbbe234bb2d04b9b20b0674889bdb45e6862
dev_demir <dev_demir@outlook.com>
2023-01-22 22:17:32 +0300

    frames and alignment

---
+++ XcodeBootcamp/FrameBootcamp.swift
@@ -0,0 +1,27 @@
+//
+//  FrameBootcamp.swift
+//  XcodeBootcamp
+//
+//  Created by Emrullah Demir on 22.01.2023.
+//
+
+import SwiftUI
+
+struct FrameBootcamp: View {
+    var body: some View {
+        Text("EMRULLAH DEMİR")
+//            .background(Color.yellow)
+//            .frame(height: 50,alignment: .top)
+//            .background(Color.red)
+//            .frame(width: 300,alignment: .trailing)
+//            .background(Color.black)
+//        
+           
+    }
+}
+
+struct FrameBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameBootcamp()
+    }
+}
