3bc8f23c077fcc1edecf47f5e12812b018be02d4
dev_demir <dev_demir@outlook.com>
2023-01-30 21:39:35 +0300

    LazyVstack and LazyHStack usage studied

--- XcodeBootcamp/ScrollViews.swift
+++ XcodeBootcamp/ScrollViews.swift
@@ -11,16 +11,16 @@ struct ScrollViews: View {
     var body: some View {
         
         ScrollView{
-            VStack{
-                ForEach(0..<20) { index in
+            LazyVStack{
+                ForEach(0..<200) { index in
                     ScrollView(.horizontal, showsIndicators: true) {
-                        HStack {
-                            ForEach(0..<10) { index in
+                        LazyHStack {
+                            ForEach(0..<300) { index in
                                 RoundedRectangle(cornerRadius: 20 )
                                     .fill(.cyan)
                                     .frame(width: 200, height: 150)
                                     .shadow(radius: 10)
-                                    .padding(10)
+                                    .padding()
                             }
                         }
                     }
