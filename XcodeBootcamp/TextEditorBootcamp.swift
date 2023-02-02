e86c27e03339835529ed53a963a25236d3e72a37
dev_demir <dev_demir@outlook.com>
2023-02-02 23:05:30 +0300

    TextEditor is the same with TextField except has multiple lines

---
+++ XcodeBootcamp/TextEditorBootcamp.swift
@@ -0,0 +1,23 @@
+//
+//  TextEditorBootcamp.swift
+//  XcodeBootcamp
+//
+//  Created by Emrullah Demir on 2.02.2023.
+//
+
+import SwiftUI
+
+struct TextEditorBootcamp: View {
+    var body: some View {
+        // Text editor is multi-lined TextField
+        
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextEditorBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextEditorBootcamp()
+    }
+}
