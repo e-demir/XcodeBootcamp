e8c28be77bcacc2ea2ff73ac7db6f254868c3268
dev_demir <dev_demir@outlook.com>
2023-02-02 21:31:05 +0300

    real world instagram-like usage example

--- XcodeBootcamp/AcrionSheetBootcamp.swift
+++ XcodeBootcamp/AcrionSheetBootcamp.swift
@@ -12,8 +12,21 @@ struct AcrionSheetBootcamp: View {
     @State var actionSheetShown : Bool = false
     
     var body: some View {
-        Button("Hello"){
-            actionSheetShown.toggle()
+        VStack{
+            HStack{
+                Circle().frame(width: 30)
+                Text("e1demir")
+                Spacer()
+                Button {
+                    actionSheetShown.toggle()
+                } label: {
+                    Image(systemName: "ellipsis").accentColor(.red)
+                }
+
+            }.padding(.horizontal)
+            
+            Rectangle().aspectRatio(contentMode: .fit)
+            
         }
         .actionSheet(isPresented: $actionSheetShown) {
             getActionSheet()
@@ -26,11 +39,12 @@ struct AcrionSheetBootcamp: View {
         let button1 : ActionSheet.Button = .default(Text("Button 1"))
         let button2 : ActionSheet.Button = .destructive(Text("Button 2"))
         let button3 : ActionSheet.Button = .default(Text("Button 3"))
+        let button4 : ActionSheet.Button = .cancel(Text("Cancel"))
                 
         return ActionSheet(
             title: Text("Title"),
             message: Text("Message"),
-            buttons: [button1,button2,button3])
+            buttons: [button1,button2,button3,button4])
     }
 }
 
