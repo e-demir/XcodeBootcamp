8a88bdd18cf7fc578403cca42b05502132ad25a3
dev_demir <dev_demir@outlook.com>
2023-02-05 17:23:49 +0300

    Picker with different styles

--- XcodeBootcamp/PickerBootcamp.swift
+++ XcodeBootcamp/PickerBootcamp.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct PickerBootcamp: View {
+    @State var selection : String = "Most liked"
+    @State var options : [String] = ["Most liked","Most popular","Recently added"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        
+        Picker(selection: $selection) {
+            ForEach(options.indices) { index in
+                Text(options[index]).tag(options[index])
+            }
+        } label: {            
+        }.pickerStyle(.segmented)
+
+        
+//        Picker(selection: $selection) {
+//            ForEach(options, id: \.self) { option in
+//                HStack {
+//                    Text(option)
+//                    Image(systemName: "heart.fill")
+//                }.tag(option)
+//
+//            }
+//        } label: {
+//
+//        }.pickerStyle(.menu)
+
+        
+//        VStack {
+//
+//            HStack{
+//                Text("Selection is: ")
+//                Text("\(selection)")
+//            }
+//            Picker(selection: $selection, label: Text("Picker"),content: {
+//                ForEach(18..<100) { number in
+//                    Text("\(number)").tag("\(number)")
+//                }
+//            })
+//            .pickerStyle(.wheel)
+////            .frame(maxWidth: .infinity)
+////                .background(Color.gray.opacity(0.3))
+//        }
+
     }
 }
 
