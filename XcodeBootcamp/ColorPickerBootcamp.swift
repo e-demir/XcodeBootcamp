fcb7f4329459f05d8ef9a39545b3fa594ee85f3a
dev_demir <dev_demir@outlook.com>
2023-02-05 17:26:28 +0300

    A simple ColorPicker added

---
+++ XcodeBootcamp/ColorPickerBootcamp.swift
@@ -0,0 +1,26 @@
+//
+//  ColorPickerBootcamp.swift
+//  XcodeBootcamp
+//
+//  Created by Emrullah Demir on 5.02.2023.
+//
+
+import SwiftUI
+
+struct ColorPickerBootcamp: View {
+    @State var selectedColor : Color = Color.red
+    
+    var body: some View {
+        ZStack{
+            selectedColor.edgesIgnoringSafeArea(.all)
+            
+            ColorPicker("Select a color", selection: $selectedColor, supportsOpacity: true)
+        }
+    }
+}
+
+struct ColorPickerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPickerBootcamp()
+    }
+}
