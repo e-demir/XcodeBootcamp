7281d92bb79145114996895406f59246254667f4
dev_demir <dev_demir@outlook.com>
2023-02-05 16:57:44 +0300

    Picker added

---
+++ XcodeBootcamp/PickerBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  PickerBootcamp.swift
+//  XcodeBootcamp
+//
+//  Created by Emrullah Demir on 5.02.2023.
+//
+
+import SwiftUI
+
+struct PickerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PickerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerBootcamp()
+    }
+}
