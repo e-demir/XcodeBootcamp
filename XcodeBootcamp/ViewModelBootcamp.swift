0672256e7adb815db1c49336ae27c6bc02bebb90
dev_demir <dev_demir@outlook.com>
2023-02-07 21:58:14 +0300

    Logic separated from the View and moved to the class

--- XcodeBootcamp/ViewModelBootcamp.swift
+++ XcodeBootcamp/ViewModelBootcamp.swift
@@ -13,8 +13,10 @@ struct FruitModel : Identifiable{
     let count: Int
 }
 
-struct ViewModelBootcamp: View {
-            
+class FruitViewModel{
+    
+    @Published var fruitArray : [FruitModel] = []
+    
     func fetchData() {
         let fruit1 = FruitModel(name: "Banana", count: 4)
         let fruit2 = FruitModel(name: "Orange", count: 1)
@@ -24,15 +26,20 @@ struct ViewModelBootcamp: View {
         fruitArray.append(fruit2)
         fruitArray.append(fruit3)
     }
+    
     func performDelete(indexSet : IndexSet){
         fruitArray.remove(atOffsets: indexSet)
     }
+
+}
+
+struct ViewModelBootcamp: View {
+    var fruitViewModel: FruitViewModel = FruitViewModel()
     
-    @State var fruitArray : [FruitModel] = []
     var body: some View {
         NavigationView {
             List{
-                ForEach(fruitArray) { fruit in
+                ForEach(fruitViewModel.fruitArray) { fruit in
                     HStack{
                         Text("\(fruit.count)")
                         Text(fruit.name)
@@ -40,13 +47,14 @@ struct ViewModelBootcamp: View {
                     }
                 }
                 .onDelete { indexSet in
-                    performDelete(indexSet: indexSet)
+                    fruitViewModel.performDelete(indexSet: indexSet)
+                }
+                .onAppear {
+                    fruitViewModel.fetchData()
                 }
             }.listStyle(.grouped)
                 .navigationTitle("Fruit List")
-                .onAppear {
-                    fetchData()
-                }
+                
         }
     }
 }
