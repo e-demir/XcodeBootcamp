3678f38222f170f9893b822a829cecc842ea5c43
dev_demir <dev_demir@outlook.com>
2023-02-11 22:26:52 +0300

    AsyncImage studied

---
+++ XcodeBootcamp/AsyncImageBootcamp.swift
@@ -0,0 +1,32 @@
+//
+//  AsyncImageBootcamp.swift
+//  XcodeBootcamp
+//
+//  Created by Emrullah Demir on 11.02.2023.
+//
+
+import SwiftUI
+
+struct AsyncImageBootcamp: View {
+    var body: some View {
+        let url = URL(string: "https://picsum.photos/500")
+        
+        AsyncImage(url: url) { returnedImage in
+            returnedImage
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .cornerRadius(20)
+        } placeholder: {
+            ProgressView()
+        }
+        
+
+    }
+}
+
+struct AsyncImageBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncImageBootcamp()
+    }
+}
